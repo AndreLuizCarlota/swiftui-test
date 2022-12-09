4b9b6bbf3a54c687f729a9b6f1566aee7dfedd2e
Andre Carlota <acarlota@lifescan.com>
2022-12-09 19:09:52 -0300

    Initial Commit

---
+++ SwiftUITests/SwiftUITestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestsApp.swift
+//  SwiftUITests
+//
+//  Created by Andre Luiz Carlota on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
